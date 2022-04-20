6b97b77248bf82875712ae7b007a083d17eed62c
Anibal Copitan <github@pprios.com>
2022-04-20 00:07:34 -0500

    [update] modularitation in the same way

--- app/application/__init__.py
+++ app/application/__init__.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from config import Config
 
-# Integration way 01
-from .home import home
-
-# Integration way 02
+from .home.routes import home_scope
 from .api.routers.api import api_scope
 from .api.routers.errors import errors_scope 
 
-# app = Flask(__name__)
 app = Flask(__name__, static_folder=Config.STATIC_FOLDER, template_folder=Config.TEMPLATE_FOLDER)
 app.config.from_object(Config)
 
 # Here register routers
-app.register_blueprint(home)
+app.register_blueprint(home_scope)
 app.register_blueprint(api_scope, url_prefix="/api")
 app.register_blueprint(errors_scope)
--- app/application/home/routes.py
+++ app/application/home/routes.py
@@ -1,13 +1,14 @@
-from flask import render_template
+from flask import render_template, Blueprint
 import requests
 
 from config import Config
-from . import home
+# from . import home
 
 API_URL = Config.API_URL
+home_scope = Blueprint("home_scope", __name__)
 
-@home.route('/')
-@home.route('/home')
+@home_scope.route('/')
+@home_scope.route('/home')
 def index():
     data = {}
     URL = API_URL + "/users/4"
